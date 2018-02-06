7101fb8a03f7dc0a267b861ac0b3bec098e797a8
ErezShv <erez.sh.sh@gmail.com>
2018-02-06 18:20:13 +0200

    remove notifications
    remove notifications on
    EnterBackground
    and
    EnterForeground

--- Tendy/AppDelegate.swift
+++ Tendy/AppDelegate.swift
@@ -608,6 +608,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate/*,CBCentralManagerDelegate
         //    stopDiscoveryTimer()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
     
     func runBackgroundTask(_ time: Int) -> Void {
@@ -659,6 +662,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate/*,CBCentralManagerDelegate
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -669,6 +673,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate/*,CBCentralManagerDelegate
         }
         connectToFcm()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        
+        
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
